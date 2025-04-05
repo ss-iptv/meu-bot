6b41a4cc054c58fc43deb91aeec3535970f81e6a
ss-iptv <96274928+ss-iptv@users.noreply.github.com>
2025-04-05 19:12:00 -0300

    .

--- deprecated/index.js
+++ deprecated/index.js
@@ -1,128 +1,41 @@
 const express = require('express');
-const socketIO = require('socket.io');
-const http = require('http');
-const { Connection, PublicKey } = require('@solana/web3.js');
-const { getAssociatedTokenAddressSync } = require('@solana/spl-token');
+const bodyParser = require('body-parser');
 const fs = require('fs');
-require('dotenv').config();
+const { Connection, PublicKey, clusterApiUrl } = require('@solana/web3.js');
 
 const app = express();
-const server = http.createServer(app);
-const io = socketIO(server);
+app.use(bodyParser.json());
 
-const PORT = process.env.PORT || 4000;
-const solanaConnection = new Connection(process.env.RPC_ENDPOINT);
+const dbPath = './db.json';
+let db = JSON.parse(fs.readFileSync(dbPath, 'utf-8'));
 
-app.use(express.json());
+const connection = new Connection(clusterApiUrl('mainnet-beta'));
 
-const readDataFromFile = (filePath) => {
-    if (fs.existsSync(filePath)) {
-        return JSON.parse(fs.readFileSync(filePath, 'utf8'));
-    }
-    return {};
-};
+// Connect Wallet Endpoint
+app.post('/connect-wallet', (req, res) => {
+  const { publicKey } = req.body;
+  if (!publicKey) return res.status(400).json({ error: 'Public key is required' });
 
-const writeDataToFile = (filePath, data) => {
-    fs.writeFileSync(filePath, JSON.stringify(data, null, 4));
-};
+  const user = { publicKey, transactions: [] };
+  db.users.push(user);
+  fs.writeFileSync(dbPath, JSON.stringify(db, null, 2));
 
-io.on('connection', (socket) => {
-    console.log(`User: ${socket.id} connected`);
-    pools();
-    initialConnection();
-
-    socket.on('disconnect', () => {
-        console.log(`User: ${socket.id} disconnected`);
-    });
-
-    socket.on('autoSell', (autoSell) => {
-        updateConfig('autoSell', autoSell);
-    });
-
-    socket.on('method', (method) => {
-        updateConfig('onceBuy', method);
-    });
-
-    socket.on('jitoMode', (jitoMode) => {
-        updateConfig('jitoMode', jitoMode);
-    });
-
-    socket.on('sell_option', (option) => {
-        updateConfig('sellOptions', option);
-    });
-
-    socket.on('filter', (option) => {
-        updateConfig('filterOptions', option);
-    });
-
-    socket.on('buy_option', (option) => {
-        updateConfig('buyOptions', option);
-    });
-
-    socket.on('jitoFee', (jitoFee) => {
-        updateConfig('jitoFee', jitoFee);
-    });
-
-    socket.on('running', (running) => {
-        updateConfig('running', running);
-    });
-
-    socket.on('autobuy', (autobuy) => {
-        updateConfig('autoBuy', autobuy);
-    });
-
-    socket.on('arbitrage', (arbitrage) => {
-        updateConfig('arbitrage', arbitrage);
-    });
-
-    socket.on('arbitrage_option', (option) => {
-        updateConfig('arbitrageOptions', option);
-    });
-
-    socket.on('mintOption', (option) => {
-        const mintingData = readDataFromFile("minting.json");
-        mintingData.wallets[option.num] = option.walletInfo;
-        writeDataToFile('minting.json', mintingData);
-    });
+  res.json({ message: 'Wallet connected', user });
 });
 
-const updateConfig = (key, value) => {
-    const data = readDataFromFile("data.json");
-    data[key] = value;
-    writeDataToFile('data.json', data);
-    broadCast(data);
-};
+// List Tokens Endpoint
+app.get('/list-tokens', async (req, res) => {
+  // Implement logic to list tokens
+  res.json({ tokens: [] });
+});
 
-const broadCast = async (data) => {
-    const balance = (await solanaConnection.getBalance(new PublicKey(data.pubKey))) / LAMPORTS_PER_SOL;
-    const wsolAddr = getAssociatedTokenAddressSync(NATIVE_MINT, new PublicKey(data.pubKey));
-    const wsolBalance = (await solanaConnection.getBalance(wsolAddr)) / LAMPORTS_PER_SOL;
-    const arbitBalance = (await solanaConnection.getBalance(new PublicKey(data.arbitPubKey))) / LAMPORTS_PER_SOL;
-    const arbitWsolAddr = getAssociatedTokenAddressSync(NATIVE_MINT, new PublicKey(data.arbitPubKey));
-    const arbitWsolBalance = (await solanaConnection.getBalance(arbitWsolAddr)) / LAMPORTS_PER_SOL;
+// Trade Token Endpoint
+app.post('/trade-token', async (req, res) => {
+  const { publicKey, token, amount } = req.body;
+  // Implement logic to trade token
+  res.json({ message: 'Trade executed' });
+});
 
-    io.emit('process', { ...data, balance, wsolBalance, arbitBalance, arbitWsolBalance });
-    tokens(data.pubKey);
-};
-
-const initialConnection = async () => {
-    const data = readDataFromFile("data.json");
-    broadCast(data);
-};
-
-const pools = async () => {
-    const pools = await returnPools();
-    const addrInfo = readDataFromFile("data.json");
-    const balance = (await solanaConnection.getBalance(new PublicKey(addrInfo.pubKey))) / LAMPORTS_PER_SOL;
-    const wsolAddr = getAssociatedTokenAddressSync(NATIVE_MINT, new PublicKey(addrInfo.pubKey));
-    const wsolBalance = (await solanaConnection.getBalance(wsolAddr)) / LAMPORTS_PER_SOL;
-
-    io.emit("pools", { pools, balance, wsolBalance });
-};
-
-const tokens = async (pubKey) => {
-    const list = await walletTokenList(pubKey);
-    io.emit("tokens", list);
-};
-
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
